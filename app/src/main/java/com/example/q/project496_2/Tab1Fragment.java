a07ed3a5da9e45529cf425f09fe425c901c113b6
cafelatte <tkdgus3078@gmail.com>
2017-07-06 19:49:14 +0900

    3개 tab 완성

---
+++ app/src/main/java/com/example/q/project496_2/Tab1Fragment.java
@@ -0,0 +1,35 @@
+package com.example.q.project496_2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by q on 2017-07-06.
+ */
+
+public class Tab1Fragment extends Fragment{
+    private static final String TAG = "tab1_Fragment";
+    private Button btnTEST;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab1_fragment, container , false);
+        btnTEST = (Button) view.findViewById(R.id.btnTest1);
+
+        btnTEST.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "BUTTON CLICK 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+
+}
