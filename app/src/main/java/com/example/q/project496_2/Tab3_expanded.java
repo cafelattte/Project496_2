8a9b8e80b026fb85502938e787c2cb4410466a01
cafelatte <tkdgus3078@gmail.com>
2017-07-11 15:02:46 +0900

    server 연결

--- app/src/main/java/com/example/q/project496_2/Tab3_expanded.java
+++ app/src/main/java/com/example/q/project496_2/Tab3_expanded.java
@@ -2,10 +2,24 @@ package com.example.q.project496_2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by q on 2017-07-10.
@@ -17,6 +31,8 @@ public class Tab3_expanded extends AppCompatActivity {
     private String major;
     private String name;
     private String student_id;
+    private EditText waitText;
+    private Button btnTest3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -32,6 +48,102 @@ public class Tab3_expanded extends AppCompatActivity {
 
         TabLayout tab = (TabLayout)findViewById(R.id.tabs);
         tab.setupWithViewPager(mViewPager);
+
+        waitText = (EditText) findViewById(R.id.waitText);
+        btnTest3 = (Button) findViewById(R.id.btnTest3);
+        btnTest3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new HttpMakeDB().execute("http://52.78.19.146:8080/lectures");
+            }
+        });
+    }
+
+    private class HttpMakeDB extends AsyncTask<String, Void, String> {
+        private boolean sendingDBcompleted = false;
+
+        @Override
+        protected void onPreExecute() {
+            waitText.setText("making DB...");
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            URL url = null;
+            HttpURLConnection connection = null;
+            String res_msg = null;
+
+            try {
+                url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("POST");
+                connection.setUseCaches(false);
+                connection.setConnectTimeout(10000);
+                connection.setRequestProperty("Content-Type", "text/plain");
+                connection.setDoOutput(true);
+
+                OutputStream os = connection.getOutputStream();
+                os.write(@string/);
+                os.close();
+
+                int resCode = connection.getResponseCode();
+                if (HttpURLConnection.HTTP_OK == resCode) {
+                    res_msg = getTextFrom(connection.getInputStream());
+                    if ("{result: 1}".equals(res_msg)) {
+                        sendingDBcompleted = true;
+                    } else if ("{result: 0".equals(res_msg)){
+                        sendingDBcompleted = false;
+                    }
+                } else {
+                    res_msg = connection.getResponseCode() + "-" + connection.getResponseMessage();
+                    sendingDBcompleted = false;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                this.sendingDBcompleted = false;
+            }
+
+            if (connection != null) {
+                connection.disconnect();
+            }
+            return res_msg;
+        }
+
+        private String getTextFrom(InputStream in) {
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new InputStreamReader(in));
+                while (true) {
+                    String line = br.readLine();
+                    if (line == null) break;
+                    sb.append(line + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return sb.toString();
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            if ("{result: 0}".equals(result)) {
+                Toast.makeText(Tab3_expanded.this, "Create DB Error", Toast.LENGTH_SHORT).show();
+                return;
+            } else if (!this.sendingDBcompleted) {
+                Toast.makeText(Tab3_expanded.this, "Sending connecting DB Error", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        Toast.makeText(Tab3_expanded.this, "Complete create DB", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void setupViewPager(ViewPager viewPager){
@@ -42,7 +154,7 @@ public class Tab3_expanded extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
-        public String getMajor(){
+    public String getMajor(){
         return major;
     }
     public String getStudentName(){
