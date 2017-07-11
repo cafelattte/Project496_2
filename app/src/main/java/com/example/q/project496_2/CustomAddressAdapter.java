ff27e0c9e6a63f3e9231edca293d580dd1ff96f4
cafelatte <tkdgus3078@gmail.com>
2017-07-12 05:09:01 +0900

    서버디비이제 다된당 ㅎㅎ 이제 현재 옛날 다들어감 respond받기까지

--- app/src/main/java/com/example/q/project496_2/CustomAddressAdapter.java
+++ app/src/main/java/com/example/q/project496_2/CustomAddressAdapter.java
@@ -2,6 +2,7 @@ package com.example.q.project496_2;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.net.Uri;
